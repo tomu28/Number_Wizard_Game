b80e8236f0a92527621f13ef5b82d231b0a34667
tomu28 <b1700662@planet.kanazawa-it.ac.jp>
2018-06-29 20:44:14 +0900

    Debug.Logの追加

--- Number Wizard Console/Assets/NumberWizard.cs
+++ Number Wizard Console/Assets/NumberWizard.cs
@@ -7,7 +7,9 @@ public class NumberWizard : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         Debug.Log("Welcome to number wizard,yo");
-
+        Debug.Log("Pick an number,don't tell me what it is...");
+        Debug.Log("The highest number you can pick is 1000");
+        Debug.Log("The lowest number you can pick is 1");
 	}
 	
 	// Update is called once per frame
